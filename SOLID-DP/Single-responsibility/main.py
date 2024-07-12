3d423f5e7ea79dd83b378fe370f98228b731b113
danofoundation <dan.dev947366@gmail.com>
2024-07-13 01:07:56 +0300

    Add single responsibility

--- SOLID-DP/Single-responsibility/main.py
+++ SOLID-DP/Single-responsibility/main.py
@@ -1,1 +1,56 @@
-print('hello')
+'''
+SRP SOC
+The single-responsibility principle (SRP) is a computer programming principle that states that "A module should be responsible to one, and only one, actor."[1] The term actor refers to a group (consisting of one or more stakeholders or users) that requires a change in the module.
+
+Source: https://en.wikipedia.org/wiki/Single-responsibility_principle
+'''
+
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+    
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f'{self.count}: {text}')
+        
+    def remove_entry(self, pos):
+        del self.entries[pos]
+        
+    def __str__(self):
+        return '\n'.join(self.entries)
+        
+   
+    #NOTE - against single responsibility design principle
+    # secondary responsibility
+    '''
+    def save(self, filename):
+        file = open(filename, 'w')
+        file.write(str(self))
+        file.close()
+    def load(self, filename):
+        pass
+    def load_from_web(self, uri):
+        pass
+    '''
+    
+    
+    #NOTE - Solution : not overload object with too many responsibilities
+class PersistenceManager():
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
+    
+    
+j = Journal()
+j.add_entry('I play tennis today')
+j.add_entry('I go go shopping in the evening')
+print(f'Journal entries: \n{j}')
+
+file = r'../../test.txt'
+PersistenceManager.save_to_file(j, file)
+
+with open(file) as fh:
+    print(fh.read())
---
+++ test.txt
@@ -0,0 +1,2 @@
+1: I play tennis today
+2: I go go shopping in the evening
